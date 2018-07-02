5e893ec4c2ea083bf37b7330b182fea31a3d0f39
Christopher Gumera <christopher.gumera@greatfeatservices.com>
2018-07-02 09:55:28 +0800

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{URL::to('css/style.css')}}">
+    </head>
+    <body>
+    @include('partials.header')
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    @yield('scripts')
+    </body>
+</html>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping Cart
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://images.penguinrandomhouse.com/cover/9780553386790" alt="no book cover">
+                <div class="caption">
+                    <h3>Game of Thrones</h3>
+                    <p class="description">
+                        Cras id neque consequat, sagittis eros in, bibendum lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum felis erat, et hendrerit nisi facilisis eget. Suspendisse congue erat lectus, sed tincidunt leo pretium eu.
+                    </p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
